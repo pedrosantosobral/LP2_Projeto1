65643e8ebf2fc73332e4fe20dc072234fc017798
Joana Marques <joana.pipaa@gmail.com>
2018-11-07 10:22:47 +0000

    Add project

---
+++ LP2_Projeto1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LP2_Projeto1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
