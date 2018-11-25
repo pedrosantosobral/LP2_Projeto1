343471e8fa77a719a7ee058b4056abd63ff5944d
Pedro Santos <pedrosantosobral@gmail.com>
2018-11-25 05:48:52 +0000

    Change Render instance

--- LP2_Projeto1/Program.cs
+++ LP2_Projeto1/Program.cs
@@ -8,9 +8,10 @@ namespace LPprojeto1MinhaCopia
         static void Main(string[] args)
         {
             ReadFile file = new ReadFile("/Users/pedrosantos/Desktop/LPprojeto1MinhaCopia/LPprojeto1MinhaCopia/games.csv");
-            Render render = new Render();
+            Render render = new Render(file.searchList);
             Filters filter = new Filters(render.selections, file.searchList);
             render.ShowMainMenu();
+
         }
     }
 }
