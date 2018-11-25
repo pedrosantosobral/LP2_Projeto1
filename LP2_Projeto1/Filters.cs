2e832885c44d6c309f054e1738b87b988404f69a
Pedro Santos <pedrosantosobral@gmail.com>
2018-11-25 10:32:23 +0000

    Add boolean type search filters

--- LP2_Projeto1/Filters.cs
+++ LP2_Projeto1/Filters.cs
@@ -5,7 +5,10 @@ namespace LPprojeto1MinhaCopia
     public class Filters
     {
         private string[] selections;
-        List<Game> searchList;
+        public List<Game> filteredList = new List<Game>();
+        public List<Game> searchList;
+        bool isRepeatedFlag = false;
+        bool i = false;
 
         public Filters(string[] selections, List<Game> searchList)
         {
@@ -17,7 +20,7 @@ namespace LPprojeto1MinhaCopia
         {
             if (selections[0].ToUpper() != "NONE")
             {
-                SearchForName();
+
             }
             if (selections[1].ToUpper() != "NONE")
             {
@@ -41,55 +44,95 @@ namespace LPprojeto1MinhaCopia
             }
             if (selections[5].ToUpper() != "NONE")
             {
-                //todo
-                //Method();
+                selections[5] = "TRUE";
             }
             if (selections[6].ToUpper() != "NONE")
             {
-                //todo
-                //Method();
+                selections[6] = "TRUE";
             }
             if (selections[7].ToUpper() != "NONE")
             {
-                //todo
-                //Method();
+                selections[7] = "TRUE";
             }
             if (selections[8].ToUpper() != "NONE")
             {
-                //todo
-                //Method();
+                selections[8] = "TRUE";
             }
             if (selections[9].ToUpper() != "NONE")
             {
-                //todo
-                //Method();
+                selections[9] = "TRUE";
             }
             if (selections[10].ToUpper() != "NONE")
             {
-                //todo
-                //Method();
+                selections[10] = "TRUE";
             }
             if (selections[11].ToUpper() != "NONE")
             {
-                //todo
-                //Method();
+                selections[11] = "TRUE";
             }
             if (selections[12].ToUpper() != "NONE")
             {
-                //todo
-                //Method();
+                selections[12] = "TRUE";
             }
             if (selections[13].ToUpper() != "NONE")
             {
-                //todo
-                //Method();
+                selections[13] = "TRUE";
             }
         }
 
-        public void SearchForName()
+        public bool SearchBool(string[] s, Game g)
         {
-            Console.WriteLine("IN");
+
+
+            if (Convert.ToBoolean(s[5]) == g.controller_support && isRepeatedFlag == false)
+            {
+                isRepeatedFlag = true;
+                filteredList.Add(g);
+            }
+            if (Convert.ToBoolean(s[6]) == g.platform_windows && isRepeatedFlag == false)
+            {
+                isRepeatedFlag = true;
+                filteredList.Add(g);
+            }
+            if (Convert.ToBoolean(s[7]) == g.platform_linux && isRepeatedFlag == false)
+            {
+                isRepeatedFlag = true;
+                filteredList.Add(g);
+            }
+            if (Convert.ToBoolean(s[8]) == g.platform_mac && isRepeatedFlag == false)
+            {
+                isRepeatedFlag = true;
+                filteredList.Add(g);
+            }
+            if (Convert.ToBoolean(s[9]) == g.category_singleplayer && isRepeatedFlag == false)
+            {
+                isRepeatedFlag = true;
+                filteredList.Add(g);
+            }
+            if (Convert.ToBoolean(s[10]) == g.category_multiplayer && isRepeatedFlag == false)
+            {
+                isRepeatedFlag = true;
+                filteredList.Add(g);
+            }
+            if (Convert.ToBoolean(s[11]) == g.category_coop && isRepeatedFlag == false)
+            {
+                isRepeatedFlag = true;
+                filteredList.Add(g);
+            }
+            if (Convert.ToBoolean(s[12]) == g.category_include_level_editor && isRepeatedFlag == false)
+            {
+                isRepeatedFlag = true;
+                filteredList.Add(g);
+            }
+            if (Convert.ToBoolean(s[13]) == g.category_vr_support && isRepeatedFlag == false)
+            {
+                isRepeatedFlag = true;
+                filteredList.Add(g);
+            }
+            return isRepeatedFlag;
         }
+
+
 
 
 
