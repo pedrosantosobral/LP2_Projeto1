fc4e6d7cd03bf015368de39d54ba2213a6753bc4
Pedro Santos <pedrosantosobral@gmail.com>
2018-11-25 00:09:43 +0000

    Add ReadFile class

--- LP2_Projeto1/ReadFile.cs
+++ LP2_Projeto1/ReadFile.cs
@@ -1,10 +1,47 @@
 ﻿using System;
-namespace LP2_Projeto1
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+namespace LPprojeto1MinhaCopia
 {
     public class ReadFile
     {
-        public ReadFile()
+        //string with the game object
+        public string[] gamesFile;
+        public HashSet<Game> allGames;
+        public List<Game> searchList = new List<Game>();
+
+        public ReadFile(string path)
         {
+            gamesFile = File.ReadAllLines(path);
+            allGames = HashSetAllGames(gamesFile);
+            searchList = allGames.ToList<Game>();
         }
+
+        //method that creates a Hashset list and recives a game object
+        public HashSet<Game> HashSetAllGames(string[] game)
+        {
+            //temporary hashset to manage game objects
+            HashSet<Game> hashTemp = new HashSet<Game>();
+
+            //go throught a game object
+            for (int i = 1; i < game.Length; i++)
+            {
+                //split the fields by comas
+                Game g = new Game(game[i].Split(','));
+
+                //add the game object to the temporary hashset
+                if (!hashTemp.Contains(g))
+                {
+                    hashTemp.Add(g);
+                }
+            }
+            //return the temporary hashset
+            return hashTemp;
+        }
+
+
+
+
     }
 }
